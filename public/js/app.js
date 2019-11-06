08e75cf4a9f57cb0626a4dddb8b34fe271a55a73
Juan Ocho <juaneme8@gmail.com>
2019-11-06 19:32:10 -0300

    Detencion del render tick cuando cierro el modal OK

--- public/js/app.js
+++ public/js/app.js
@@ -8,15 +8,22 @@ let myReq = 0;
 
 document.addEventListener('DOMContentLoaded', () => {
   const modals = document.querySelectorAll('.modal');
+  //Inicializo todos los modals
   M.Modal.init(modals, {
     onOpenStart: function(aux) {
-      //Si el modal inicializado corresponde al scan de QR llamo a nuevoCodigo()
+      //Si se trata del modal de scan llamo a nuevoCodigo()
       if (aux.id === 'modal-scan') {
         nuevoCodigo();
+      }
+    },
+    onCloseStart: function(aux) {
+      if (aux.id === 'modal-scan') {
+        window.cancelAnimationFrame(myReq);
       }
     }
   });
 
+  //Side menu ubicado a la derecha.
   const mobile = document.querySelector('#mobile-demo');
   M.Sidenav.init(mobile, { edge: 'right' });
 
@@ -33,39 +40,49 @@ document.addEventListener('DOMContentLoaded', () => {
   const tick = () => {
     loadingMessage.innerText = '⌛ Cargando video...';
     let lecturaOK = false;
+    //Si tengo suficientes datos
     if (video.readyState === video.HAVE_ENOUGH_DATA) {
+      //Oculto el mensaje de cargando
       loadingMessage.hidden = true;
+      //Muestro el canvas
       canvas.hidden = false;
-
+      //Defino las dimensiones del canvas
       canvas.height = video.videoHeight;
       canvas.width = video.videoWidth;
+      //Establezco el contenido del canvas
       canvas2d.drawImage(video, 0, 0, canvas.width, canvas.height);
+      //Obtentgo una imagen de la porción de video mostrada en este instante
       const imageData = canvas2d.getImageData(0, 0, canvas.width, canvas.height);
+      //Chequeo si hay un código presente en esa imagen
       const codigoLeido = jsQR(imageData.data, imageData.width, imageData.height, {
         inversionAttempts: 'dontInvert'
       });
+      //Si lei un código válido.
       if (codigoLeido) {
-        //Código leído
+        //Dibujo una rectángulo alrededor del código qr
         dibujoLinea(codigoLeido.location.topLeftCorner, codigoLeido.location.topRightCorner, '#07bab2');
         dibujoLinea(codigoLeido.location.topRightCorner, codigoLeido.location.bottomRightCorner, '#07bab2');
         dibujoLinea(codigoLeido.location.bottomRightCorner, codigoLeido.location.bottomLeftCorner, '#07bab2');
         dibujoLinea(codigoLeido.location.bottomLeftCorner, codigoLeido.location.topLeftCorner, '#07bab2');
 
+        //Cargo el textbox con el valor de qr obtenido
         qrLeido.value = codigoLeido.data;
 
         //Cierro modal
         const modalScan = document.querySelector('#modal-scan');
-        M.modal.getInstance(modalScan).close();
+        M.Modal.getInstance(modalScan).close();
 
         //Coloco lecturaOK y cancelo animación de modo de dar por terminada la secuencia.
         lecturaOK = true;
         window.cancelAnimationFrame(myReq);
       } else {
+        //Si no leí un código limpio el textbox
         qrLeido.value = '';
       }
     }
+    //Si lecturaOK es false redibujo la pantalla
     if (!lecturaOK) {
-      //requestAnimationFrame nos permite redibujar la pantalla cada vez detectamos un nuevo QR.
+      //requestAnimationFrame nos permite redibujar la pantalla
       myReq = requestAnimationFrame(tick);
     }
   };
@@ -74,13 +91,13 @@ document.addEventListener('DOMContentLoaded', () => {
     // Use facingMode: environment to attemt to get the front camera on phones
     navigator.mediaDevices
       .getUserMedia({ video: { facingMode: 'environment' } })
-      .then((stream) => {
+      .then(stream => {
         video.srcObject = stream;
         video.setAttribute('playsinline', true); // required to tell iOS safari we don't want fullscreen
         video.play();
         myReq = requestAnimationFrame(tick);
       })
-      .catch((err) => {
+      .catch(err => {
         console.log('getUserMedia Error // ' + err);
       });
   };
