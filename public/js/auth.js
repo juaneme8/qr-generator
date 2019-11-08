d02bf332e679cb7ce0e779d6b4d56b693b49b896
Juan Ocho <juaneme8@gmail.com>
2019-11-08 19:54:37 -0300

    Fusion completa con algunos pendientes2

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,16 +1,18 @@
-//SIGN UP
+//CREACIÓN DE USUARIO
 const signupForm = document.querySelector('#signup-form');
-signupForm.addEventListener('submit', (e) => {
+signupForm.addEventListener('submit', e => {
   e.preventDefault();
 
   // Obtener info de usuario
   const email = signupForm['signup-email'].value;
   const password = signupForm['signup-password'].value;
 
-  // Registro de nuevo usuario & add firestore data
+  // Registro de nuevo usuario
   auth
     .createUserWithEmailAndPassword(email, password)
-    .then((cred) => {
+    .then(cred => {
+      console.log('Usuario Creado');
+      //Agrego a la DB en la colección usuarios, la bio con el mismo id que fue entregado al usuario
       return db
         .collection('usuarios')
         .doc(cred.user.uid)
@@ -18,129 +20,123 @@ signupForm.addEventListener('submit', (e) => {
           bio: signupForm['signup-bio'].value
         });
     })
+    //Una vez creado el usuario cierro el modal y limpio los campos
     .then(() => {
+      //Obtengo una referencia del modal
       const modal = document.querySelector('#modal-signup');
       //Cierro el modal
       M.Modal.getInstance(modal).close();
       //Limpio el formulario
       signupForm.reset();
-      //Limpio el mensaje de error.
+      //Limpio el mensaje de error en caso de que lo hubiera.
       signupForm.querySelector('.error').innerHTML = '';
     })
-    .catch((err) => {
-      //Muestro mensaje de error
+    .catch(err => {
+      //Muestro el mensaje de error obtenido
       signupForm.querySelector('.error').innerHTML = err.message;
-    });
-});
-
-//LOGIN
-const loginForm = document.querySelector('#login-form');
-loginForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  // get user info
-  const email = loginForm['login-email'].value;
-  const password = loginForm['login-password'].value;
-
-  // log the user in
-  auth
-    .signInWithEmailAndPassword(email, password)
-    .then((cred) => {
-      // close the signup modal & reset form
-      const modal = document.querySelector('#modal-login');
-      M.Modal.getInstance(modal).close();
-      loginForm.reset();
-      loginForm.querySelector('.error').innerHTML = '';
-    })
-    .catch((err) => {
-      loginForm.querySelector('.error').innerHTML = err.message;
     });
 });
 
 // LOGOUT
 const logout = document.querySelector('#logout');
-logout.addEventListener('click', (e) => {
+logout.addEventListener('click', e => {
+  console.log('Usuario Desconectado');
   e.preventDefault();
   auth.signOut();
 });
 
-// LISTEN AUTH CHANGES
-auth.onAuthStateChanged((user) => {
-  if (user) {
-    user.getIdTokenResult().then((idTokenResult) => {
-      user.admin = idTokenResult.claims.admin;
-      //setupUI(user);
+//LOGIN
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  // Obtengo los datos del usuario del formulario
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value;
+
+  auth
+    .signInWithEmailAndPassword(email, password)
+    .then(cred => {
+      console.log('Usuario Conectado');
+      // Obtengo una referencia del modal
+      const modal = document.querySelector('#modal-login');
+      //Cierro el modal
+      M.Modal.getInstance(modal).close();
+      //Limpio el formulario
+      loginForm.reset();
+      //Limpio los errores pues el login fue exitoso
+      loginForm.querySelector('.error').innerHTML = '';
+    })
+    .catch(err => {
+      //Muestro el mensaje de error obtenido
+      loginForm.querySelector('.error').innerHTML = err.message;
     });
-    db.collection('equipos').onSnapshot(
-      (snapshot) => {
-        setupGuides(snapshot.docs);
+});
+
+// LISTEN AUTH CHANGES
+auth.onAuthStateChanged(user => {
+  console.log('onAuthStateChanged: ' + user);
+  //Si estoy logueado
+  if (user) {
+    user.getIdTokenResult().then(idTokenResult => {
+      //Cargo el valor del custom claim
+      user.admin = idTokenResult.claims.admin;
+      renderUI(user);
+    });
+    //Obtengo todos los equipos
+    /*db.collection('equipos').onSnapshot(
+      snapshot => {
+        renderEquipos(snapshot.docs);
       },
-      (err) => console.log(err.message)
-    );
+      err => console.log(err.message)
+    );*/
   } else {
-    //setupUI();
-    //setupGuides([]);
+    //Si no estoy logueado, limpio los datos que sólo los usuarios logueados pueden ver
+    renderUI();
+    //renderEquipos([]);
   }
 });
 
-// AGREGAR INSTRUMENTO
-const createForm = document.querySelector('#create-form');
-createForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  db.collection('equipos')
-    .add({
-      titulo: createForm.titulo.value,
-      descripcion: createForm.descripcion.value
-    })
-    .then(() => {
-      // close the create modal & reset form
-      console.log('Insturmento agregado');
-      const modal = document.querySelector('#modal-create');
-      M.Modal.getInstance(modal).close();
-      createForm.reset();
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
-});
-
 // REAL-TIME LISTENER
-db.collection('equipos').onSnapshot((snapshot) => {
-  snapshot.docChanges().forEach((change) => {
+db.collection('equipos').onSnapshot(snapshot => {
+  snapshot.docChanges().forEach(change => {
     if (change.type === 'added') {
       renderEquipo(change.doc.data(), change.doc.id);
     }
     if (change.type === 'removed') {
-      renderEquipo(change.doc.id);
+      removeEquipo(change.doc.id);
     }
   });
 });
 
-// AGREGAR MATERIAL
-const form = document.querySelector('form');
-form.addEventListener('submit', (evt) => {
+// AGREGAR EQUIPO
+const agregarEquipo = document.querySelector('#agregar-equipo');
+agregarEquipo.addEventListener('submit', evt => {
   evt.preventDefault();
 
-  const recipe = {
-    name: form.title.value,
-    ingredients: form.ingredients.value
+  //Conformo el objeto que guardaré
+  const equipo = {
+    id: agregarEquipo.id.value,
+    sector: agregarEquipo.sector.value
   };
 
-  db.collection('recipes')
-    .add(recipe)
-    .catch((err) => console.log(err));
+  //Agrego equipo a la DB
+  db.collection('equipos')
+    .add(equipo)
+    .catch(err => console.log(err));
 
-  form.title.value = '';
-  form.ingredients.value = '';
+  //Limpio el formulario
+  agregarEquipo.id.value = '';
+  agregarEquipo.sector.value = '';
 });
 
-// ELIMINAR MATERIAL
-const recipeContainer = document.querySelector('.equipos');
-recipeContainer.addEventListener('click', (evt) => {
+// ELIMINAR EQUIPO
+const contenedorEquipos = document.querySelector('.contenedorEquipos');
+contenedorEquipos.addEventListener('click', evt => {
   if (evt.target.tagName === 'I') {
     const id = evt.target.getAttribute('data-id');
     //console.log(id);
-    db.collection('recipes')
+    db.collection('equipos')
       .doc(id)
       .delete();
   }
--- public/js/ui.js
+++ public/js/ui.js
@@ -1,4 +1,9 @@
-const equipos = document.querySelector('.equipos');
+//todo para evitar dubplicado
+//const contenedorEquipos = document.querySelector('.contenedorEquipos');
+const accountDetails = document.querySelector('.account-details');
+const adminItems = document.querySelectorAll('.admin');
+const loggedOutLinks = document.querySelectorAll('.logged-out');
+const loggedInLinks = document.querySelectorAll('.logged-in');
 
 document.addEventListener('DOMContentLoaded', function() {
   // nav menu
@@ -9,25 +14,60 @@ document.addEventListener('DOMContentLoaded', function() {
   M.Sidenav.init(forms, { edge: 'left' });
 });
 
-// render recipe data
-const renderRecipe = (data, id) => {
+const renderUI = user => {
+  //Si el usuario está logueado
+  if (user) {
+    //Si es admin
+    if (user.admin) {
+      //Muestro todos los elementos sólo visibles por admin
+      adminItems.forEach(item => (item.style.display = 'block'));
+    }
+    // Obtengo los datos de firestore (no de Auth)
+    db.collection('usuarios')
+      .doc(user.uid)
+      .get()
+      .then(doc => {
+        const html = `
+        <div>Logged in as ${user.email}</div>
+        <div>${doc.data().bio}</div>
+        <div class="pink-text">${user.admin ? 'Admin' : ''}</div>
+      `;
+        accountDetails.innerHTML = html;
+      });
+    // Muestro los elementos visibles para usuarios logueados y oculto los de usuarios deslogueados.
+    loggedInLinks.forEach(item => (item.style.display = 'block'));
+    loggedOutLinks.forEach(item => (item.style.display = 'none'));
+  }
+  //El usuario no está logueado
+  else {
+    // Limpio la información
+    accountDetails.innerHTML = '';
+    // toggle user elements
+    adminItems.forEach(item => (item.style.display = 'none'));
+    loggedInLinks.forEach(item => (item.style.display = 'none'));
+    loggedOutLinks.forEach(item => (item.style.display = 'block'));
+  }
+};
+
+//RENDER EQUIPO
+const renderEquipo = (data, id) => {
   const html = `
     <div class="card-panel recipe white row" data-id="${id}">
-      <img src="/img/dish.svg" alt="recipe thumb">
+      <img src="/img/dish.svg" alt="foto de equipo">
       <div class="recipe-details">
-        <div class="recipe-title">${data.name}</div>
-        <div class="recipe-ingredients">${data.ingredients}</div>
+        <div class="recipe-title">${data.id}</div>
+        <div class="recipe-ingredients">${data.descripcion}</div>
       </div>
       <div class="recipe-delete">
         <i class="material-icons" data-id="${id}">delete_outline</i>
       </div>
     </div>
   `;
-  equipos.innerHTML += html;
+  contenedorEquipos.innerHTML += html;
 };
 
-// remove recipe
-const removeRecipe = (id) => {
-  const recipe = document.querySelector(`.recipe[data-id=${id}]`);
-  recipe.remove();
+//ELIMINAR EQUIPO
+const removeEquipo = id => {
+  const equipo = document.querySelector(`.recipe[data-id=${id}]`);
+  equipo.remove();
 };
